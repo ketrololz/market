15413dcc9604ea42a3f4118b4a11a10c7b7736a9
ketrololz <bqnexxx@gmail.com>
2025-04-29 15:45:58 +0300

    chore: install tanstack query

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+
 import './style.css';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.mount('#app');
+app.use(VueQueryPlugin);
