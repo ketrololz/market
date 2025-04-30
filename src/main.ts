c68c61ebbeaac677b26d1e2775d5f924364014ee
ketrololz <bqnexxx@gmail.com>
2025-04-30 18:27:42 +0300

    chore: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import './style.css';
 import App from './App.vue';
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(PrimeVue);
-app.mount('#app');
 app.use(VueQueryPlugin);
+app.use(pinia);
+app.mount('#app');
