d7881cf679451e52379f7f9e74a307bd736657f1
ketrololz <bqnexxx@gmail.com>
2025-05-07 22:22:48 +0300

    feat: create preset

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import { Button } from 'primevue';
-</script>
-
 <template>
-  <div class="flex">
-    <Button label="Save" icon="pi pi-check" iconPos="right" />
-    <Button label="Search" icon="pi pi-search" iconPos="top" />
-    <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
-  </div>
+  <div>Market</div>
 </template>
