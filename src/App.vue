3d9bafc466b8ae202bce605e89e781d600d74805
ketrololz <bqnexxx@gmail.com>
2025-05-07 21:27:02 +0300

    feat: install primevue icons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from 'primevue';
+</script>
 
 <template>
-  <div>Market</div>
+  <div class="flex">
+    <Button label="Save" icon="pi pi-check" iconPos="right" />
+    <Button label="Search" icon="pi pi-search" iconPos="top" />
+    <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
+  </div>
 </template>
