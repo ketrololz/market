af596e53deae7a4d4f75581629bc4e34d38ba479
Daniil Sudorgin <ddsudorgin@mail.ru>
2025-05-08 17:49:22 +0300

    chore: configure environment variable handling

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,19 @@
+/// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+  readonly VITE_CT_PROJECT_KEY: string;
+  readonly VITE_CT_CLIENT_ID: string;
+  readonly VITE_CT_CLIENT_SECRET: string;
+  readonly VITE_CT_API_URL: string;
+  readonly VITE_CT_AUTH_URL: string;
+  readonly VITE_CT_SCOPES: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
