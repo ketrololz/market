6a87f53a8f4bc07e3883eef98208bd5c3848511e
ketrololz <bqnexxx@gmail.com>
2025-05-10 18:59:48 +0300

    chore: install vite inline svg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,12 @@ const items = ref([
 
 <template>
   <Menubar :model="items">
+    <template #start>
+      <svg width="35" height="40" viewBox="0 0 35 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-8">
+            <path d="..." fill="var(--p-primary-color)" />
+            <path d="..." fill="var(--p-text-color)" />
+        </svg>
+    </template>
     <template #item="{ item, props, hasSubmenu }">
       <RouterLink v-if="item.route" :to="item.route" v-bind="props.action">
         <span v-if="item.icon" :class="item.icon"></span>
