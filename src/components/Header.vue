12b764d33be98e31be00f1866262f5e159e8c20c
ketrololz <bqnexxx@gmail.com>
2025-05-10 23:25:24 +0300

    feat: add user login button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { Menubar } from 'primevue';
+import { Menu } from 'primevue';
+import { Button } from 'primevue';
 import { ref } from 'vue';
 import InlineSvg from 'vue-inline-svg';
 
@@ -10,17 +12,25 @@ const items = ref([
     icon: 'pi pi-home',
   },
 ]);
+
+const userItems = ref([
+  {
+    label: 'Login',
+    route: '/',
+    icon: 'pi pi-home',
+  },
+]);
+
+const menu = ref();
+const toggle = (event: Event) => {
+  menu.value.toggle(event);
+};
 </script>
 
 <template>
   <Menubar :model="items">
     <template #start>
-      <InlineSvg
-        src="logo-main.svg"
-        height="26"
-        aria-label="logo"
-      >
-      </InlineSvg>
+      <InlineSvg src="logo-main.svg" height="26" aria-label="logo"> </InlineSvg>
     </template>
     <template #item="{ item, props, hasSubmenu }">
       <RouterLink v-if="item.route" :to="item.route" v-bind="props.action">
@@ -32,6 +42,19 @@ const items = ref([
         <span>{{ item.label }}</span>
         <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down"></span>
       </a>
+    </template>
+    <template #end>
+      <Button
+        type="button"
+        class="ml-0"
+        icon="pi pi-user"
+        aria-haspopup="true"
+        aria-controls="overlay_menu"
+        size="small"
+        variant="outlined"
+        @click="toggle"
+      />
+      <Menu id="overlay_menu" ref="menu" :model="userItems" :popup="true" />
     </template>
   </Menubar>
 </template>
