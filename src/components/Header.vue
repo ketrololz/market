06b022d3308292e9739ee8d89c00d6011b5c1fa7
ketrololz <bqnexxx@gmail.com>
2025-05-10 18:31:36 +0300

    feat: create template for links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
-  import { Menubar } from 'primevue';
-  import { ref } from 'vue';
+import { Menubar } from 'primevue';
+import { ref } from 'vue';
 
-  const items = ref([
-    {
-      label: 'Main',
-      route : '/',
-      icon: 'pi pi-home',
-    }
-  ])
+const items = ref([
+  {
+    label: 'Main',
+    route: '/',
+    icon: 'pi pi-home',
+  },
+]);
 </script>
 
 <template>
   <Menubar :model="items">
     <template #item="{ item, props, hasSubmenu }">
-      <RouterLink  v-if="item.route" :to="item.route">
+      <RouterLink v-if="item.route" :to="item.route" v-bind="props.action">
         <span v-if="item.icon" :class="item.icon"></span>
         <span>{{ item.label }}</span>
       </RouterLink>
-      <span v-else>{{ item.label }}</span>
+      <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+        <span v-if="item.icon" :class="item.icon"></span>
+        <span>{{ item.label }}</span>
+        <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down"></span>
+      </a>
     </template>
   </Menubar>
 </template>
