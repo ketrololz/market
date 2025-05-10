a338cda288fbb264a264360c467900556df89249
ketrololz <bqnexxx@gmail.com>
2025-05-10 18:10:13 +0300

    feat: add link to main page

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue';
+</script>
+
 <template>
+  <Header></Header>
   <RouterView></RouterView>
 
   <!-- Use the following template to add a link to the page:
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { Menubar } from 'primevue';
+  import { ref } from 'vue';
+
+  const items = ref([
+    {
+      label: 'Main',
+      route : '/',
+      icon: 'pi pi-home',
+    }
+  ])
+</script>
+
+<template>
+  <Menubar :model="items">
+    <template #item="{ item, props, hasSubmenu }">
+      <RouterLink  v-if="item.route" :to="item.route">
+        <span v-if="item.icon" :class="item.icon"></span>
+        <span>{{ item.label }}</span>
+      </RouterLink>
+      <span v-else>{{ item.label }}</span>
+    </template>
+  </Menubar>
+</template>
