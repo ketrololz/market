dc310fa5d6cdd3f8f4be8a20315d33a7aea3ca25
ketrololz <bqnexxx@gmail.com>
2025-05-13 00:25:40 +0300

    404 page
    * feat: create 404 page
    
    * feat: add redirect to 404 page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,3 +1,4 @@
+import NotFound from '../page/NotFound.vue'
 import type { RouteRecordRaw } from 'vue-router';
 import RegistrationPage from '../pages/RegistrationPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
@@ -11,5 +12,5 @@ export const ROUTES: RouteRecordRaw[] = [
   // { path: '/', component: Main },
   { path: '/login', component: LoginPage },
   { path: '/registration', component: RegistrationPage },
-  // { path: '/404', component: NotFound },
+  { path: '/:pathMatch(.*)*', component: NotFound }, // Должен всегда быть последним
 ];
