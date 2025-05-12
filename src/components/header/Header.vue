7788faa8d9b3c9475af4b36080d8f3b291e88460
ketrololz <bqnexxx@gmail.com>
2025-05-13 00:44:53 +0300

    Header component
    * feat: add link to main page
    
    * feat: create template for links
    
    * chore: install vite inline svg
    
    * feat: add logo
    
    * feat: add user login button
    
    * feat: add login, sign up and logout icons
    
    * feat: add props
    
    * feat: add links constant

---
+++ src/components/header/Header.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Menubar } from 'primevue';
+import { Menu } from 'primevue';
+import { Button } from 'primevue';
+import { ref } from 'vue';
+import InlineSvg from 'vue-inline-svg';
+import type { HeaderProps } from './types/header-props';
+
+defineProps<HeaderProps>();
+
+const menu = ref();
+const toggle = (event: Event) => {
+  menu.value.toggle(event);
+};
+</script>
+
+<template>
+  <Menubar :model="navList">
+    <template #start>
+      <InlineSvg src="logo-main.svg" height="26" aria-label="logo"> </InlineSvg>
+    </template>
+    <template #item="{ item, props, hasSubmenu }">
+      <RouterLink v-if="item.route" :to="item.route" v-bind="props.action">
+        <span v-if="item.icon" :class="item.icon"></span>
+        <span>{{ item.label }}</span>
+      </RouterLink>
+      <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+        <span v-if="item.icon" :class="item.icon"></span>
+        <span>{{ item.label }}</span>
+        <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down"></span>
+      </a>
+    </template>
+    <template #end>
+      <Button
+        type="button"
+        class="ml-0"
+        icon="pi pi-user"
+        aria-haspopup="true"
+        aria-controls="overlay_menu"
+        size="small"
+        variant="outlined"
+        @click="toggle"
+      />
+      <Menu
+        id="overlay_menu"
+        ref="menu"
+        :model="userLoginOptions"
+        :popup="true"
+      />
+    </template>
+  </Menubar>
+</template>
---
+++ src/components/header/types/header-props.ts
@@ -0,0 +1,6 @@
+import type { MenuItem } from 'primevue/menuitem';
+
+export interface HeaderProps {
+  navList: MenuItem[],
+  userLoginOptions: MenuItem[],
+}
