2f37cd665e6b3af53435a66cce9edea116d9f3f7
ketrololz <bqnexxx@gmail.com>
2025-05-16 15:08:08 +0300

    feat: implement footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <RouterView></RouterView>
-
   <!-- Use the following template to add a link to the page:
   <RouterLink to="/link-example">Some text</RouterLink> -->
 </template>
-
-<script lang="ts">
-export default {};
-</script>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <footer
+    class="flex flex-col items-center justify-center gap-y-3 pt-8 pb-10 bg-slate-900"
+  >
+    <div>
+      <RouterLink to="/">
+        <InlineSvg
+          src="logo-inversion.svg"
+          height="22"
+          aria-label="logo"
+          class="mb-5"
+        >
+        </InlineSvg>
+      </RouterLink>
+    </div>
+    <p class="text-indigo-400 font-medium text-lg">Our Team</p>
+    <ul class="flex gap-x-4">
+      <li>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/YaroshenkoAnna"
+          class="text-white font-medium hover:text-indigo-400 transition duration-100 ease-in-out"
+          >YaroshenkoAnna</a
+        >
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/zilusion"
+          class="text-white font-medium hover:text-indigo-400 transition duration-100 ease-in-out"
+          >zilusion</a
+        >
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/ketrololz"
+          class="text-white font-medium hover:text-indigo-400 transition duration-100 ease-in-out"
+          >ketrololz</a
+        >
+      </li>
+    </ul>
+    <p class="text-white text-sm">2025 RPG Heroes</p>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import InlineSvg from 'vue-inline-svg';
+import { RouterLink } from 'vue-router';
+</script>
