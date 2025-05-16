b7d7a43618c0a9a750233c186dc012da89aec808
ketrololz <bqnexxx@gmail.com>
2025-05-16 20:20:06 +0300

    refactor: add test for footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Footer from './components/footer/Footer.vue';
+</script>
 
 <template>
   <RouterView></RouterView>
+  <Footer></Footer>
   <!-- Use the following template to add a link to the page:
   <RouterLink to="/link-example">Some text</RouterLink> -->
 </template>
