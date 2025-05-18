86622d8c145d26925afe738782e111adf7cb2a30
ketrololz <bqnexxx@gmail.com>
2025-05-18 15:59:56 +0300

    feat: add header and implement user logout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Footer from './components/footer/Footer.vue';
+import Header from './components/header/Header.vue';
+import { NAV_LINKS } from './components/header/constants/nav-links.ts';
+import { USER_NAV_LINKS } from './components/header/constants/user-login-options.ts';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
 
 <template>
+  <Header :nav-list="NAV_LINKS" :user-login-options="USER_NAV_LINKS"></Header>
   <RouterView></RouterView>
   <Footer v-if="route.name !== '404'"></Footer>
   <!-- Use the following template to add a link to the page:
