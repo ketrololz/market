e9b119532fc4b6408ac926b29bdf3e8d32dcc18a
ketrololz <bqnexxx@gmail.com>
2025-05-24 21:58:10 +0300

    60 interactive product cards
    * refactor: make routes dynamic
    
    * feat: add catalog page
    
    * feat: create base product card template
    
    * feat: add some styles
    
    * feat: define card props
    
    * fix: add lazy imports to router
    
    * feat: add routing
    
    * test: add product card component test
    
    * test: add product card test
    
    * refactor: remove div from product card component

---
+++ src/pages/catalog-page/CatalogPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import ProductCard from '../../components/product-card/ProductCard.vue';
+
+const cardInfo = reactive({
+  image: '/images/products/explosive-cats/1.webp',
+  title: 'Взрывные котята',
+  description:
+    '"Взрывные котята" – это карточная игра, дико популярная на "Кикстартере".',
+  price: '990 ₽',
+  isAvailable: true,
+  route: '/',
+});
+</script>
+
+<template>
+  <div class="w-full h-full flex items-center justify-center">
+    <ProductCard :card-info="cardInfo" />
+  </div>
+</template>
