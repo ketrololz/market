761c3bc290c17261b9afd0e45cf2f8479cece495
ketrololz <bqnexxx@gmail.com>
2025-05-25 17:50:35 +0300

    Category navigation (#64)
    * feat: add categories
    
    * feat: add breadcrumb
    
    * feat: change some styles
    
    * refactor: remove players

--- src/pages/catalog-page/CatalogPage.vue
+++ src/pages/catalog-page/CatalogPage.vue
@@ -1,6 +1,14 @@
+<!-- TODO:
+      - search
+      - pagination
+      - filters
+      - scroll animation -->
+
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import ProductCard from '../../components/product-card/ProductCard.vue';
+import Sidebar from '../../components/sidebar/Sidebar.vue';
+import { Breadcrumb } from 'primevue';
 
 const cardInfo = reactive({
   image: '/images/products/explosive-cats/1.webp',
@@ -11,10 +19,33 @@ const cardInfo = reactive({
   isAvailable: true,
   route: '/',
 });
+
+const home = ref({ label: 'Home', route: '/' });
+
+const items = ref([{ label: 'All games' }]);
+
+function handleSwitch(category: { name: string; code: string }) {
+  items.value = [{ label: category.name }];
+}
 </script>
 
 <template>
-  <div class="w-full h-full flex items-center justify-center">
-    <ProductCard :card-info="cardInfo" />
+  <div class="px-4 relative">
+    <Breadcrumb :home="home" :model="items">
+      <template #item="{ item }">
+        <RouterLink :to="item.route">
+          <span class="text-(--p-breadcrumb-item-color)">{{ item.label }}</span>
+        </RouterLink>
+      </template>
+    </Breadcrumb>
+    <div class="flex gap-10 flex-col md:flex-row min-h-150">
+      <Sidebar
+        class="static top-4 md:sticky"
+        @switch-category="handleSwitch"
+      ></Sidebar>
+      <div class="w-full h-full flex items-center justify-center">
+        <ProductCard :card-info="cardInfo" />
+      </div>
+    </div>
   </div>
 </template>
