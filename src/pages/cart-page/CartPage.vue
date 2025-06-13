75b4e83b87162fe713efecf12f541001240174fd
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-06-13 12:09:41 +0200

    style: add responsive design for cart page

--- src/pages/cart-page/CartPage.vue
+++ src/pages/cart-page/CartPage.vue
@@ -12,17 +12,11 @@ import { Form } from '@primevue/forms';
 import Divider from 'primevue/divider';
 import { onMounted, computed } from 'vue';
 import { useCartStore } from '@/stores/cartStore';
-import Accordion from 'primevue/accordion';
-import AccordionPanel from 'primevue/accordionpanel';
-import AccordionHeader from 'primevue/accordionheader';
-import AccordionContent from 'primevue/accordioncontent';
-import { useProductCacheStore } from '@/stores/productCacheStore';
 import { useRouter } from 'vue-router';
 import ConfirmDialog from 'primevue/confirmdialog';
 import { useConfirm } from 'primevue/useconfirm';
 
 const cartStore = useCartStore();
-const productCacheStore = useProductCacheStore();
 const router = useRouter();
 const confirm = useConfirm();
 
@@ -48,21 +42,30 @@ onMounted(async () => {
   );
 });
 
+function getPlayersText(item) {
+  const min = item.variant.attributes?.find(
+    (a) => a.name === 'players-min',
+  )?.value;
+  const max = item.variant.attributes?.find(
+    (a) => a.name === 'players-max',
+  )?.value;
+
+  if (min && max) return `${min} - ${max}`;
+  if (min) return `${min}+`;
+  if (max) return `up to ${max}`;
+
+  return null;
+}
+
 const cartItems = computed(() => {
   return (
     cartStore.cart?.lineItems.map((item) => {
       return {
         id: item.id,
         name: item.name.en,
-        players: `${
-          item.variant.attributes?.find((a) => a.name === 'players-min')?.value
-        } - ${
-          item.variant.attributes?.find((a) => a.name === 'players-max')?.value
-        }`,
-        age: `${
-          item.variant.attributes?.find((a) => a.name === 'age-recommended')
-            ?.value
-        }+`,
+        players: getPlayersText(item),
+        age: item.variant.attributes?.find((a) => a.name === 'age-recommended')
+          ?.value,
         playTime: item.variant.attributes?.find(
           (a) => a.name === 'playing-time-min',
         )?.value,
@@ -78,9 +81,6 @@ const cartItems = computed(() => {
         price: item.totalPrice.centAmount / 100,
         imageUrl: item.variant.images?.[0]?.url ?? '',
         productId: item.productId,
-        description:
-          productCacheStore.getProductDescriptionSync?.(item.productId, 'en') ??
-          '',
       };
     }) ?? []
   );
@@ -149,7 +149,9 @@ const showClearCartDialog = () => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 p-4">
+  <div
+    class="flex flex-col gap-4 p-4 max-w-[1536px] mx-auto w-full max-sm:px-0"
+  >
     <Panel
       header="My Order"
       pt:header:class="justify-self-center text-xl pb-0!"
@@ -181,61 +183,81 @@ const showClearCartDialog = () => {
       <p v-else>Your order ships free of charge</p>
     </Panel>
 
-    <Panel v-if="cartItems.length > 0" pt:header:class="pb-0!">
+    <Panel v-if="cartItems.length > 0" pt:header:class="p-0!">
       <DataTable :value="cartItems" class="w-full">
-        <Column field="item" header="Item">
+        <Column field="item" header="Item" header-class="max-md:text-sm">
           <template #body="slotProps">
             <div
-              class="block w-20 h-20 cursor-pointer hover:scale-105 transition duration-300 ease-in-out"
+              class="block w-20 h-20 cursor-pointer hover:scale-105 transition duration-300 ease-in-out mx-auto max-sm:w-16 max-sm:h-16"
               @click="() => router.push(`/product/${slotProps.data.productId}`)"
             >
               <img
                 :src="slotProps.data.imageUrl"
                 alt="Product Image"
-                class="object-contain w-full h-full"
+                class="object-contain h-full"
               />
             </div>
           </template>
         </Column>
-        <Column field="description" header="Description">
+        <Column
+          field="description"
+          header="Description"
+          header-class="max-md:text-sm max-md:w-[160px] md:w-[250px] lg:w-[320px]"
+        >
           <template #body="slotProps">
-            <div class="flex flex-col">
-              <span
-                class="text-sm text-primary font-medium text-(--p-primary-color) cursor-pointer hover:underline"
-                @click="navigate(`/product/${slotProps.data.productId}`)"
-              >
-                {{ slotProps.data.name }}
-              </span>
-              <span class="text-xs text-gray-500">
-                <strong>Number of players:</strong> {{ slotProps.data.players }}
-              </span>
-              <span class="text-xs text-gray-500">
-                <strong>Age:</strong> {{ slotProps.data.age }}
-              </span>
-              <span class="text-xs text-gray-500">
-                <strong>Min. Playing Time:</strong>
-                {{ slotProps.data.playTime }}
-              </span>
-              <span class="text-xs text-gray-500">
-                <strong>Publisher:</strong> {{ slotProps.data.publisher }}
-              </span>
-
-              <Accordion value="0" unstyled="true">
-                <AccordionPanel value="1">
-                  <AccordionHeader>
-                    <strong class="text-xs text-gray-500">Details</strong>
-                  </AccordionHeader>
-                  <AccordionContent>
-                    <p class="text-xs text-gray-500">
-                      {{ slotProps.data.description }}
-                    </p>
-                  </AccordionContent>
-                </AccordionPanel>
-              </Accordion>
+            <div class="flex">
+              <div class="flex flex-col items-start text-left">
+                <span
+                  class="text-sm text-primary font-medium text-(--p-primary-color) cursor-pointer hover:underline text-ellipsis overflow-hidden"
+                  style="
+                    display: -webkit-box;
+                    -webkit-line-clamp: 1;
+                    line-clamp: 1;
+                    -webkit-box-orient: vertical;
+                  "
+                  @click="
+                    () => router.push(`/product/${slotProps.data.productId}`)
+                  "
+                >
+                  {{ slotProps.data.name }}
+                </span>
+                <span
+                  v-if="slotProps.data.players"
+                  class="text-xs text-gray-500"
+                >
+                  <strong>Number of players:</strong>
+                  {{ slotProps.data.players }}
+                </span>
+                <span
+                  v-if="
+                    (slotProps.data.age !== undefined &&
+                      slotProps.data.age !== '') ||
+                    slotProps.data.age === 0
+                  "
+                  class="text-xs text-gray-500"
+                >
+                  <strong>Age:</strong> {{ slotProps.data.age }}<span>+</span>
+                </span>
+                <span
+                  v-if="slotProps.data.playTime"
+                  class="text-xs text-gray-500"
+                >
+                  <strong>Min. Playing Time:</strong>
+                  {{ slotProps.data.playTime }}
+                </span>
+                <span class="text-xs text-gray-500">
+                  <strong>Publisher:</strong> {{ slotProps.data.publisher }}
+                </span>
+              </div>
             </div>
           </template>
         </Column>
-        <Column field="quantity" header="Quantity">
+        <Column
+          field="quantity"
+          header="Quantity"
+          header-class="max-md:text-sm max-sm:hidden"
+          body-class="max-sm:hidden"
+        >
           <template #body="slotProps">
             <InputNumber
               v-model="slotProps.data.quantity"
@@ -246,19 +268,23 @@ const showClearCartDialog = () => {
               :max="100"
               size="small"
               allow-empty="false"
-              input-class="w-10 text-center !px-0"
-              increment-button-class="!px-1 !w-6"
-              decrement-button-class="!px-1 !w-6"
+              input-class="w-10 text-center !px-0 max-md:!text-xs"
+              increment-button-class="!px-1 !w-6 max-md:!hidden"
+              decrement-button-class="!px-1 !w-6 max-md:!hidden"
               @update:model-value="onQuantityChange(slotProps.data)"
             />
           </template>
         </Column>
-        <Column header="Unit Price">
+        <Column
+          header="Unit Price"
+          header-class="max-md:text-sm max-sm:hidden"
+          body-class="max-sm:hidden"
+        >
           <template #body="slotProps">
-            <div class="flex flex-col items-start">
+            <div class="flex flex-col items-center text-sm max-md:text-xs">
               <span
                 v-if="slotProps.data.hasDiscount"
-                class="line-through text-sm text-gray-500"
+                class="line-through text-xs text-gray-500 max-md:text-[10px]"
               >
                 {{ slotProps.data.originalUnitPrice.toFixed(2) }} €
               </span>
@@ -266,8 +292,75 @@ const showClearCartDialog = () => {
             </div>
           </template>
         </Column>
-        <Column field="price" header="Price" />
-        <Column header="">
+        <Column
+          header="Price"
+          header-class="max-md:text-sm max-sm:hidden"
+          body-class="max-sm:hidden"
+        >
+          <template #body="slotProps">
+            <div class="text-center text-sm max-md:text-xs">
+              {{ slotProps.data.price.toFixed(2) }} €
+            </div>
+          </template>
+        </Column>
+
+        <Column
+          header=""
+          header-class="hidden max-sm:table-cell max-sm:text-sm"
+          body-class="hidden max-sm:table-cell"
+        >
+          <template #body="slotProps">
+            <div class="flex flex-col gap-1 text-xs text-center">
+              <div>
+                <InputNumber
+                  v-model="slotProps.data.quantity"
+                  :step="1"
+                  :min="1"
+                  :max="100"
+                  size="small"
+                  allow-empty="false"
+                  input-class="w-9 text-center !px-1 !py-0 !text-xs"
+                  increment-button-class="!hidden"
+                  decrement-button-class="!hidden"
+                  @update:model-value="onQuantityChange(slotProps.data)"
+                />
+              </div>
+              <div>
+                <span class="flex flex-col justify-center items-center">
+                  <span
+                    v-if="slotProps.data.hasDiscount"
+                    class="line-through text-gray-500 text-[10px] mr-1"
+                    >{{ slotProps.data.originalUnitPrice.toFixed(2) }} €</span
+                  >
+                  <span>{{ slotProps.data.unitPrice.toFixed(2) }} €</span>
+                </span>
+              </div>
+              <div>
+                <span class="text-nowrap"
+                  >{{ slotProps.data.price.toFixed(2) }} €</span
+                >
+              </div>
+            </div>
+            <div>
+              <Button
+                icon="pi pi-trash"
+                class="p-button-rounded"
+                size="small"
+                :pt="{
+                  root: { class: '!w-5 !h-5 !p-1' },
+                  icon: { class: '!text-xs' },
+                }"
+                @click="onDelete(slotProps.data.id)"
+              />
+            </div>
+          </template>
+        </Column>
+
+        <Column
+          header=""
+          header-class="max-md:text-sm max-sm:hidden"
+          body-class="max-sm:hidden"
+        >
           <template #body="slotProps">
             <Button
               icon="pi pi-trash"
@@ -287,16 +380,16 @@ const showClearCartDialog = () => {
         <Button label="Apply" class="w-1/2 max-w-[10rem]" size="small"
       /></Form>
 
-      <div class="flex justify-between">
+      <div class="flex justify-between max-sm:text-sm">
         <span>Shopping Cart ( {{ totalQuantity }} Item )</span>
         <span>{{ totalPrice.toFixed(2) }} €</span>
       </div>
-      <div class="flex justify-between">
+      <div class="flex justify-between max-sm:text-sm">
         <span>Discount with Promocode</span>
         <span> 0 €</span>
       </div>
       <Divider />
-      <div class="flex justify-between">
+      <div class="flex justify-between font-bold">
         <span>Total</span>
         <span> {{ totalPrice.toFixed(2) }} €</span>
       </div>
@@ -323,12 +416,11 @@ const showClearCartDialog = () => {
 </template>
 
 <style scoped>
-.p-accordionheader {
-  display: flex;
-  gap: 0.5rem;
-}
-
-.p-datatable-column-header-content {
+::v-deep(.p-datatable-column-header-content) {
   justify-content: center;
+}
+::v-deep(.p-datatable-tbody > tr > td) {
+  text-align: center;
+  padding: 0.75rem 0.3rem;
 }
 </style>
