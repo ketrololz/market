fbc6a558bcb707f0d8bbe35f697711c68dfeafa6
Bogdan Nurgatin <bnurgatin@dpg.email>
2025-06-14 19:37:03 +0300

    style: set routerview max width

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,11 @@ const route = useRoute();
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen justify-between">
+  <div class="flex flex-col min-h-screen justify-start">
     <Header :nav-list="NAV_LINKS" :user-login-options="USER_NAV_LINKS"></Header>
-    <RouterView></RouterView>
-    <Footer v-if="route.name !== '404'"></Footer>
+    <div class="flex flex-1 justify-center">
+      <RouterView class="flex-1 max-w-[1920px]"></RouterView>
+    </div>
+    <Footer v-if="route.name !== '404'" class=""></Footer>
   </div>
 </template>
